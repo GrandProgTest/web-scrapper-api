b8d3216d11f18e26ff9d1183c8c744d0091d258d
GrandProgTest <u202124343@upc.edu.pe>
2024-08-25 20:24:08 -0500

    feat: better search has been implemented

--- app.py
+++ app.py
@@ -49,5 +49,33 @@ def search_ofac():
 
     return jsonify({"hits": hits, "results": filtered_data})
 
+@app.route('/search_bank_more_open', methods=['GET'])
+@limiter.limit("20 per minute")
+def search_bank_more_open():
+    firm_name = request.args.get('firm_name')
+
+    if not firm_name:
+        return jsonify({"error": "No firm name provided"}), 400
+
+    data = main("worldbank")
+    filtered_data = [firm for firm in data if firm_name.lower() in firm["Firm Name"].lower()]
+    hits = len(filtered_data)
+
+    return jsonify({"hits": hits, "results": filtered_data})
+
+@app.route('/search_ofac_more_open', methods=['GET'])
+@limiter.limit("20 per minute")
+def search_ofac_more_open():
+    firm_name = request.args.get('firm_name')
+
+    if not firm_name:
+        return jsonify({"error": "No firm name provided"}), 400
+
+    data = main("ofac", firm_name)
+    filtered_data = [firm for firm in data if firm_name.lower() in firm["Name"].lower()]
+    hits = len(filtered_data)
+
+    return jsonify({"hits": hits, "results": filtered_data})
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
